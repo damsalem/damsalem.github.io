607107cc6e1281514d0ff7c42ddaee54ae892336
Dani Amsalem <dani@theamsalems.com>
2023-03-12 00:08:24 -0500

    Update Portfolio Markup and Files

--- js/main.js
+++ js/main.js
@@ -1,21 +1,14 @@
 // Used by typed.min.js
 const typed = new Typed("#typed", {
-  stringsElement: "#typed-strings",
-  startDelay: 500,
-  typeSpeed: 100,
-  backSpeed: 50,
-  backDelay: 1200,
-  shuffle: true,
-  loop: true
+	stringsElement: "#typed-strings",
+	startDelay: 500,
+	typeSpeed: 100,
+	backSpeed: 50,
+	backDelay: 1200,
+	shuffle: true,
+	loop: true,
 });
 
-// Toggles Animista's Shake Class
-let thumbnails = document.querySelectorAll(".shields-ctn img");
+const year = document.querySelector("#year");
 
-function addHover() {
-  this.classList.toggle("shake-bottom");
-}
-
-// Event listeners to add/remove the class
-thumbnails.forEach(thumb => thumb.addEventListener("mouseover", addHover));
-thumbnails.forEach(thumb => thumb.addEventListener("mouseout", addHover));
+year.innerHTML = new Date().getFullYear();
